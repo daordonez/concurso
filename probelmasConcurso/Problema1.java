f70b10d2c9145bcb5ee4bc396b912e9a1463634c
Diego Ordonez <diegordonez4@gmail.com>
2015-03-12 16:57:47 +0100

    Problema C

--- probelmasConcurso/Problema1.java
+++ probelmasConcurso/Problema1.java
@@ -2,8 +2,23 @@
 import java.util.*;
 public class Problema1 {
 
+    static Scanner tec = new Scanner(System.in);
 
+    //Problema B
     public static void main(String[] args) {
+
+        //Casos prueba
+        int N = tec.nextInt();
+
+        //Numero de lineas
+        int M = tec.nextInt();
+
+        boolean imprime = false;
+
+        for (int i = 0; i < M ; i++) {
+             String cadTemp = tec.nextLine();
+        }
+
 
     }
 }
--- probelmasConcurso/Problema2.java
+++ probelmasConcurso/Problema2.java
@@ -2,7 +2,75 @@
 import java.util.*;
 
 public class Problema2 {
+
+    //Problema E
+    static Scanner tec = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        //Precios
+        int p1 = tec.nextInt();
+        int p2 = tec.nextInt();
+        int p3 = tec.nextInt();
+        int p4 = tec.nextInt();
+
+        int vec[] = new int[2];
+
+        vec[0] = tec.nextInt();
+        vec[1] = tec.nextInt();
+
+
+
+    }
+
+    public static int b1(int vec[], int p1) {
+
+        int precio = 0;
+
+         precio = (vec[0] * p1) + (vec[1] * p1);
+
+        return precio;
+    }
+
+    public static int b2(int vec[], int p2) {
+
+        int precio = 0;
+
+        if (vec[0] > 0){
+            precio += p2 * vec[0];
+        }
+
+        if (vec[1] > 0){
+            precio += p2 * vec[1];
+        }
+
+        return precio;
+    }
+
+    public static int b3(int vec[], int p2) {
+
+        int precio = 0;
+
+        if (vec[0] > 0){
+            precio += p2;
+        }
+
+        if (vec[1] > 0){
+            precio += p2;
+        }
+
+        return precio;
+    }
+
+    public static int b4(int vec[], int p2) {
+
+        int precio = 0;
+
+        if (vec[0] != 0 || vec[1] != 0){
+
+            precio = p2;
+        }
+
+        return precio;
     }
 }
